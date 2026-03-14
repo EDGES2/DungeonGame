4ed0c44b34936de0bf98f93d2ed0d993a382bb9f
EDGES2 <kirilltokarev826@gmail.com>
2026-03-14 20:55:56 +0100

    Basic idle animation and MapTileLayer added

--- src/scripts/Player.cs
+++ src/scripts/Player.cs
@@ -7,9 +7,13 @@ public partial class Player : CharacterBody2D
 {
     [Export] public float speed = 150f;
 
+    private AnimatedSprite2D _playerSprite2D;
+
     public void GetInput()
     {
         Vector2 direction = Input.GetVector("left", "right", "up", "down");
+
+        //Movement handling
         if (direction != Vector2.Zero)
         {
             Velocity = direction * speed;
@@ -18,6 +22,12 @@ public partial class Player : CharacterBody2D
         {
             Velocity = Velocity.MoveToward(Vector2.Zero, speed);
         }
+
+        //Direction change
+        if (direction.X > 0)
+            _playerSprite2D.FlipH = false;
+        else if (direction.X < 0)
+            _playerSprite2D.FlipH = true;
         Vector2 velocity = Velocity;
         velocity = direction * speed;
         Velocity = velocity;
@@ -26,6 +36,7 @@ public partial class Player : CharacterBody2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        _playerSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
     }
     public override void _PhysicsProcess(double delta)
     {
