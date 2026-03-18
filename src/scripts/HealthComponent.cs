bb4a47d45e2b600bbed3306951bed9124632e371
EDGES2 <kirilltokarev826@gmail.com>
2026-03-18 15:59:53 +0100

    hit animation added

--- src/scripts/HealthComponent.cs
+++ src/scripts/HealthComponent.cs
@@ -7,18 +7,30 @@ namespace Game.Components;
 public partial class HealthComponent : Node2D
 {
 	[Export] public float MaxHealth = 100.0f;
+	[Export] private AnimationPlayer anim;
+
 	private float _currentHealth;
+	private bool _isDead = false;
 
 	public override void _Ready()
 	{
 		_currentHealth = MaxHealth;
+		anim.AnimationFinished += OnAnimationFinished;
 	}
 
 	public void Damage(Attack attack)
 	{
 		_currentHealth -= attack.Damage;
+		anim.Play("take_hit");
 
 		if (_currentHealth <= 0)
+		{
+			_isDead = true;
+		}
+	}
+	private void OnAnimationFinished(StringName animName)
+	{
+		if (_isDead && animName == "take_hit")
 		{
 			GetParent().QueueFree();
 		}
